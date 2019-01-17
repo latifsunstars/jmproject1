01b35623b33758efcc910b28c27c1ecadb62b913
latifsunstars <latif.sunstars@gmail.com>
2019-01-17 14:51:11 +0530

    Hello Java

---
+++ src/main/java/com/myvedic/jmproject1/hellojava.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myvedic.jmproject1;
+
+/**
+ *
+ * @author student1
+ */
+public class hellojava {
+    public static void main(String[] args) {
+      System.out.println("Hello!Java\nThis is my 1st project"); 
+    }
+}
